9dcec3fb3767f6e234b27e1b752f4d9c7d556dcd
HeySearra <heysearra@163.com>
2023-07-26 16:27:20 +0800

    merge2

--- merge2.py
+++ merge2.py
@@ -4,7 +4,37 @@ import numpy as np
 import sys
 import pandas as pd
 import json
+from tqdm import tqdm
+from dict import merge_dict
 
+data = pd.read_excel('./data/pre/merge_1_1.xlsx')
 
-data_0 = pd.read_csv('./data/./data/pre/merge_1.xlsx')
+item_map = {}  # 防止重复双主键 (PatientID & Time)
+patients_data_list = []
+patients_temp = {}
+key_list = []
+for _, i in tqdm(data.iterrows(), total=data.shape[0]):
+    patient_item = {}
+    if str(i['userId']) + i['actualExamineDate'] not in item_map:
+        item_map[str(i['userId']) + i['actualExamineDate']] = 1
+    else:
+        patient_item = patients_temp[str(i['userId']) + i['actualExamineDate']]
+    key = merge_dict[i['sectionID']][i['itemId']]
+    if key not in key_list:
+        key_list.append(key)
+    patient_item['userId'] = i['userId'] if 'userId' not in patient_item.keys() else patient_item['userId']
+    patient_item['department'] = i['department'] if 'department' not in patient_item.keys() else patient_item['department']
+    patient_item['formID'] = i['formID'] if 'formID' not in patient_item.keys() else patient_item['formID']
+    patient_item[key] = i['itemValue'] if key not in patient_item.keys() else patient_item[key]
+    patient_item['actualExamineDate'] = i['actualExamineDate'] if 'actualExamineDate' not in patient_item.keys() else patient_item['actualExamineDate']
+    patient_item['userName'] = i['userName'] if 'userName' not in patient_item.keys() else patient_item['userName']
+    patient_item['gender'] = i['gender'] if 'gender' not in patient_item.keys() else patient_item['gender']
+    patient_item['birthday'] = i['birthday'] if 'birthday' not in patient_item.keys() else patient_item['birthday']
+    patient_item['diyCode'] = i['diyCode'] if 'diyCode' not in patient_item.keys() else patient_item['diyCode']
+    patient_item['userTag'] = i['userTag'] if 'userTag' not in patient_item.keys() else patient_item['userTag']
 
+    patients_temp[str(i['userId']) + i['actualExamineDate']] = patient_item
+
+# for
+df_excel = pd.read_json(json.dumps(list(patients_temp.values()), ensure_ascii=False))
+df_excel.to_excel('./data/pre/merge_2.xlsx', index=False)
